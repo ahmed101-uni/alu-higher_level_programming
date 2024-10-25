048e8113fa1b08eb4b07356f04c15ce39416af4c
ahmed101-uni <a.khalid@alustudent.com>
2024-10-24 20:36:59 -0500

    hot

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
 # why does it need to be 8 lines? To give space to use variables. Still not doing it the "intended way", screw you pep8
-[first, last, middle] = [word[:3], word[-2:], word[1:-1]]
+first = word[:3]
+last = word[-2:]
+middle = word[1:-1]
 
 print(f"First 3 letters: {first}")
 print(f"Last 2 letters: {last}")
