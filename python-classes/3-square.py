8605839fe61c078c4f0d7952083c0a9c6b58b9a6
ahmed101-uni <a.khalid@alustudent.com>
2024-11-11 12:43:35 -0600

    task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the square, and validate.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        """
+        return self.__size**2
