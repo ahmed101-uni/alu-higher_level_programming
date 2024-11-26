2b352a13e225030a4fad15ea9714507b9d72ee03
ahmed101-uni <a.khalid@alustudent.com>
2024-11-26 16:25:56 -0600

    task1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,13 +3,29 @@
 This script fetches the status from a given URL using the urllib library.
 """
 
+
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as request:
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
-        data = request.read()
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+
+# import urllib.request
+
+# if __name__ == "__main__":
+#     url = "https://alu-intranet.hbtn.io/status"
+#     with urllib.request.urlopen(url) as request:
+#         print("Body response:")
+#         data = request.read()
+#         print("\t- type: {}".format(type(data)))
+#         print("\t- content: {}".format(data))
+#         print("\t- utf8 content: {}".format(data.decode("utf-8")))
