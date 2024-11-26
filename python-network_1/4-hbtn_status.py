924b6095188e693e394f42ae1d29c80491a33276
ahmed101-uni <a.khalid@alustudent.com>
2024-11-26 16:26:10 -0600

    task4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """
-    This script makes a request and displays a
-    response using the requests package.
+This script makes a request and displays a
+response using the requests package.
 """
 
 import requests
 
-if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
+
+
+# if __name__ == "__main__":
+#     response = requests.get("https://alu-intranet.hbtn.io/status")
+
+#     print("Body response:")
+#     print(f"\t- type: {type(response.text)}")
+#     print(f"\t- content: {response.text}")
