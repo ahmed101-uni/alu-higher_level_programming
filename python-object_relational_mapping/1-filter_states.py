157cbbf4be159c32aeb596397e2f7cf53a59330a
ahmed101-uni <a.khalid@alustudent.com>
2024-12-01 09:46:22 -0600

    task1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that connects to a MySQL database, retrieves
+rows from the 'states' table where name starts with "N",
+and prints them in ascending order by 'id'.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbName,
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
